e4b9edf1ca804c05785be64c424a6febb52e7fdc
Harpi <id.harpi@pm.me>
2023-03-16 05:57:02 +0700

    create frontend DTO

---
+++ HotelRealtaPayment.Contract/Models/FrontEnd/JSONCollection.cs
@@ -0,0 +1,7 @@
+﻿namespace HotelRealtaPayment.Contract.Models.FrontEnd;
+
+public class JSONCollection<T>
+{
+    public string? status { get; set; }
+    public Dictionary<string, List<T>> data { get; set; }
+}
